81010355cabd15ddd7262e3560a1ae4edb8c75e9
VolrahQ <m.popov@xlab-game.com>
2024-11-12 11:13:57 +0300

    AddScripts

---
+++ Assets/scripts/GameData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+    public int money;
+    public int MinSellTaskPrice;
+    public int MaxSellTaskPrice;
+    public int BuyTaskPrice;
+    public int TableUpgrade;
+    public int BuyTable;
+}
