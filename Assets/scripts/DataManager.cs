8562df2bafa9518dc1a1ec1965ab20c967fbf3d7
Mikhail Popov <m.popov@xlab-game.com>
2024-11-15 00:03:21 +0300

    Refactoring

--- Assets/scripts/DataManager.cs
+++ Assets/scripts/DataManager.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Data;
-using UnityEditor;
 using UnityEngine;
 
 public class DataManager : MonoBehaviour
--- Assets/scripts/MoneyManager.cs
+++ Assets/scripts/MoneyManager.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class MoneyManager : MonoBehaviour
 {
-    public void AddMoney(int item)
+    public static MoneyManager Instance { get; private set; }
+
+
+    // Событие для обновления UI
+    public delegate void OnMoneyChanged(int currentMoney);
+    public event OnMoneyChanged MoneyChanged;
+
+    private void Awake()
     {
-       DataManager.money += item;
-       DataManager.Save();
-    }
-    public void RemoveMoney(int item)
-    {
-        DataManager.money -= item;
-        DataManager.Save();
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
+    public void AddMoney(int amount)
+    {
+        if (amount > 0)
+        {
+            DataManager.money += amount;
+            MoneyChanged?.Invoke(DataManager.money);
+        }
+    }
+
+    public bool SpendMoney(int amount)
+    {
+        if (amount > 0 && DataManager.money >= amount)
+        {
+            DataManager.money -= amount;
+            MoneyChanged?.Invoke(DataManager.money);
+            return true;
+        }
+        return false;
+    }
+
+    public bool CanAfford(int amount)
+    {
+        return DataManager.money >= amount;
+    }
 }
